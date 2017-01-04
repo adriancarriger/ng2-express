6fd0cee783634bb3e9e10e966734f1d11ec30671
Adrian Carriger <mail@adrianandnicole.com>
2017-01-04 11:38:21 -0800

    feature(server): add express

---
+++ server/routes/test.ts
@@ -0,0 +1,12 @@
+import { Router, Response, Request } from 'express';
+
+const testRouter: Router = Router();
+
+testRouter.get('/test', (request: Request, response: Response) => {
+  response.json({
+    title: 'Greetings.',
+    text: 'Hello Angular 2'
+  });
+});
+
+export { testRouter }
